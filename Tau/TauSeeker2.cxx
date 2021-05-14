6b76d322a1578a479eac145683b02118a9aa6469
brusale <brusale11@gmail.com>
2021-05-14 18:42:35 +0200

    added half-life, good job

--- Tau/TauSeeker2.cxx
+++ Tau/TauSeeker2.cxx
@@ -1,3 +1,4 @@
+#include <cmath>
 //
 //	***********************************************
 //
@@ -16,7 +17,7 @@ void TauSeeker2() {
 
 	// load spectrum without bkg due to random coincidences
 	TFile* file = new TFile("./TauClean.root");
-	TH1F* tau = (TH1F*)file->Get("tau_clean_low");
+	TH1F* tau = (TH1F*)file->Get("tau_clean");
 
 	//fit only the right part of the spectrum with an exponential distribution
 	TF1* exponential = new TF1("exponential", "[3]+[0]*exp(-(x-[1])/[2])", 2300, 4700);
@@ -41,14 +42,14 @@ void TauSeeker2() {
 	exponential->SetParameter(2, 300);
 	exponential->SetParameter(3, 5);
 
-	tau->Fit(exponential, "RL");
+	tau->Fit(exponential, "R");
 
 
 	float t = exponential->GetParameter(2);
 	float t_0 = exponential->GetParameter(1);
 
 	//define fit function
-	TF1* convolution = new TF1("convolution", "[0]+[1]/2*exp(-(x-[2])/[3]+[4]/(2*[3]*[3]))*(1+TMath::Erf((x-[2]-[4]/[3])/(sqrt(2*[4]))))", 1000, 4700);
+	TF1* convolution = new TF1("convolution", "[0]+[1]/2*exp(-(x-[2])/[3]+[4]/(2*[3]*[3]))*(1+TMath::Erf((x-[2]-[4]/[3])/(sqrt(2*[4]))))", 1500, 4700);
 
 	convolution->SetParName(0, "A");
 	convolution->SetParName(1, "C");
@@ -60,7 +61,8 @@ void TauSeeker2() {
 	convolution->SetParameter(1, 150);
 	convolution->SetParameter(2, t_0);
 	convolution->SetParameter(3, t);
-	convolution->FixParameter(4, 140*140);
+	convolution->SetParameter(4, 80*80);
+
 
 	tau->SaveAs("TauSeeker.root");
 	tau->Fit(convolution, "R");
@@ -75,11 +77,18 @@ void TauSeeker2() {
 
 	float tauvalue = (channel_tau - q)/m;
 
+	std::cout << "------ TAU ------" << std::endl;
 	std::cout << "\n57Fe lifetime: " << tauvalue << std::endl;
 
 	float sigma_tau = 1/pow(m, 2)*sqrt(pow((channel_tau - q),2)*sigma_m*sigma_m + sigma_q*sigma_q);
 	std::cout << "error: " << sigma_tau << std::endl;
 
+	std::cout << "\n------ HALF-LIFE ------" << std::endl;
+	float half_life = log(2)*tauvalue;
+	float sigma_half_life = log(2)*sigma_tau;
+
+	std::cout << "\n57Fe half-life: " << half_life << std::endl;
+	std::cout << "error: " << sigma_half_life << std::endl;
 	//residuals
 	/*Double_t channel[9000], residuals[9000];
 	for (int i = 0; i < 9000; i++) {
